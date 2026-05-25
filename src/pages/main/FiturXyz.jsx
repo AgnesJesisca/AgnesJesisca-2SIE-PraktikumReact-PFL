b7984c84b8dbe80e7ad14f2499b0d96d03afc6cb
Agnes Jesisca <agnes24si@mahasiswa.pcr.ac.id>
2026-05-25 15:59:15 +0700

    penambahan fitur shadcnui

--- src/pages/main/FiturXyz.jsx
+++ src/pages/main/FiturXyz.jsx
@@ -1,3 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+ } from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { IconFolderCode } from "@tabler/icons-react";
+import { ArrowUpRightIcon } from "lucide-react";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+ import { Badge } from "@/components/ui/badge";
 import PageHeader from "../../components/PageHeader";
 
 export default function FiturXyz() {
@@ -9,6 +35,94 @@ export default function FiturXyz() {
       />
 
       <p className="mt-4">Ini Halaman Fitur Xyz</p>
+
+      <Button variant="outline">Contoh Tombol Outline</Button>
+      <Button variant="destructive">Contoh Tombol Destructive</Button>
+      <Button variant="default">Contoh Tombol Deafult</Button>
+      <Button variant="ghost">Contoh Tombol Ghost</Button>
+      <Button variant="link">Contoh Tombol Link</Button>
+      <Button variant="secondary">Contoh Tombol Secondary</Button>
+
+            <Card className="mt-4 w-[380px]">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>Belajar shadcn/ui</CardTitle>
+            <Badge variant="secondary">Baru</Badge>
+          </div>
+          <CardDescription>
+            Contoh penggunaan komponen shadcn/ui di React
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p className="text-sm text-muted-foreground">
+            Komponen ini dibuat di branch <strong>setup-shadcn</strong>
+            lalu di-merge ke main.
+          </p>
+        </CardContent>
+
+        <CardFooter className="flex gap-2">
+          <Button>Simpan</Button>
+          <Button variant="outline">Batal</Button>
+        </CardFooter>
+      </Card>
+
+      <Accordion
+      type="single"
+      collapsible
+      defaultValue="shipping"
+      className="max-w-lg"
+    >
+      <AccordionItem value="shipping">
+        <AccordionTrigger>What are your shipping options?</AccordionTrigger>
+        <AccordionContent>
+          We offer standard (5-7 days), express (2-3 days), and overnight
+          shipping. Free shipping on international orders.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="returns">
+        <AccordionTrigger>What is your return policy?</AccordionTrigger>
+        <AccordionContent>
+          Returns accepted within 30 days. Items must be unused and in original
+          packaging. Refunds processed within 5-7 business days.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="support">
+        <AccordionTrigger>How can I contact customer support?</AccordionTrigger>
+        <AccordionContent>
+          Reach us via email, live chat, or phone. We respond within 24 hours
+          during business days.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+
+
+      <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <IconFolderCode />
+        </EmptyMedia>
+        <EmptyTitle>No Projects Yet</EmptyTitle>
+        <EmptyDescription>
+          You haven&apos;t created any projects yet. Get started by creating
+          your first project.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent className="flex-row justify-center gap-2">
+        <Button>Create Project</Button>
+        <Button variant="outline">Import Project</Button>
+      </EmptyContent>
+      <Button
+        variant="link"
+        asChild
+        className="text-muted-foreground"
+        size="sm"
+      >
+        <a href="#">
+          Learn More <ArrowUpRightIcon />
+        </a>
+      </Button>
+    </Empty>
     </div>
   );
 }
